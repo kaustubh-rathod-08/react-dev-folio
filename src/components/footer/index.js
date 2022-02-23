96f289bb042d7f13d792de2f21cd72f2b6bf16d6
Vedant Yetekar <ursvedantyetekar@gmail.com>
2022-02-23 18:14:33 +0530

    changed heading color and updated footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.css";
 
 function Footer() {
-  return <div className="footer">&copy;Vedant Yetekar</div>;
+  return <div className="footer">Copyright &copy; Vedant Yetekar</div>;
 }
 
 export default Footer;
